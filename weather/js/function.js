93100e1601d34f7637b7ffc272ff244e1617cb7f
smainoo <mai15005@byui.edu>
2018-03-10 23:19:43 -0700

    s

--- weather/js/function.js
+++ weather/js/function.js
@@ -10,9 +10,10 @@ const speed = 5;
 buildWC(speed, temp);
 
 
+
 // This function will calculate the wind chill temperature
 function buildWC(speed, temp){
-const feelTemp = document.getElementById('feel');
+const feelTemp = document.getElementById('feelTemp');
 
   //calculate windchill
   let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
