9a679f5f47348f1718263e4448da1944c68a74d5
smainoo <mai15005@byui.edu>
2018-03-10 23:05:18 -0700

    s

--- weather/js/function.js
+++ weather/js/function.js
@@ -15,7 +15,7 @@ function buildWC(speed, temp){
 const feelTemp = document.getElementById('feel');
 
   //calculate windchill
-  let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(sped, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+  let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
   console.log(wc);
 
   //Round the answer down to integer
