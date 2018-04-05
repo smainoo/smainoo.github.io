9f5dc9b0f02a56bcdc6e3558a804f19b95d8ba8d
smainoo <mai15005@byui.edu>
2018-04-05 11:11:44 -0600

    s

--- acme/acme/js/acme.js
+++ acme/acme/js/acme.js
@@ -1,28 +1,69 @@
-'use strict';
-
-document.getElementById("home").addEventListener("click", dispalyData);
-
-function displayData(){
-document.getElementById("home").innerHTML
-
-                                               });
+const JSONDATA = "/acme/js/acme.json";
+let jsonData;
+getData(JSONDATA);
 
 //get data from json
 
 
-function getData(){
-  const URL = "/js."
-  fetch("")
+function getData(data){
+  fetch(data)
   .then(response => response.json())
-  .then(function(navigation){
+  .then(function(data){
     console.log('json object from getData function:');
-    console.log(navigation);
+    console.log(data);
+    jsonData = data;
+    processJSON(data);
   })
   .catch(error => console.log('There was an error: ', error))
   }
+//end get data function
 
-//dispaly function
-function dispalyData(navigation){
-  const anvil = navigation.
+//process JSON function
+function processJSON(data){
+  //print the data returned
+  console.log(data);
+
+//for loop to list the navigation links
+  let list = "<ul>";
+  for (let i = 0, n = 5; i < n; i++){
+    list += "<li>" + data.navarray[i] + "</li>";
+  };
+  list += "</li>";
+  pageNav.innerHTML = list;
 
 }
+
+//create a variable to store page-nave list
+const NAVLINKS = document.getElementById("pageNav");
+
+//add event listener
+NAVLINKS.addEventListener("click", function(event){
+  document.getElementById("home").innerHTML="Acme |Homepage";
+
+  //hide and show content upon click
+
+  const HIDEHOME = document.getElementById("");
+  const SHOWCONTENT = document.getElementById("");
+  const CONTENTIMAGE = document.getElementById("");
+
+  let content = event.target.innerHTML;
+  console.log(content);
+
+  //switch content based on links click
+
+  if (content == ""){
+    document.getElementById("home").innerHTML="Acme |Homepage";
+    SHOWCONTENT.setAttribute("class", "content-hide");
+    HIDEHOME.setAttribute("class", "home-show");
+  }
+
+  else if (content == "Anvils") {
+    document.getElementById("acme-title").innerHTML="Acme | Anvils";
+    META.setAttribute("content", "Acme Anvils");
+    HOMEHIDE.setAttribute("class", "home-hide");
+    CONSHOW.setAttribute("class", "content-show");
+    console.log(jsonData);
+
+
+}
+})
